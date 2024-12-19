95fc9f95628e3a45734cfde839a9a58e159735d1
csedgedelta <114102375+csedgedelta@users.noreply.github.com>
2024-12-19 11:23:51 -0600

    Update build.sh

--- build/ed-gen-long/build.sh
+++ build/ed-gen-long/build.sh
@@ -1,3 +1,3 @@
-docker build -t chadtsigler/ed-gen-long:1.0.1 -t chadtsigler/ed-gen-long:latest .
-docker push chadtsigler/ed-gen-long:1.0.1
+docker build -t chadtsigler/ed-gen-long:1.0.2 -t chadtsigler/ed-gen-long:latest .
+docker push chadtsigler/ed-gen-long:1.0.2
 docker push chadtsigler/ed-gen-long:latest
